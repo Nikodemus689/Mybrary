2e9085bb3996a330a313d20dc9ae5036d78a77d6
Nikodemus689 <ninim@interia.pl>
2023-07-06 19:07:16 +0200

    Initial Author Routes

--- server.js
+++ server.js
@@ -6,15 +6,23 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayout = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
-
+const authorsRouter = require('./routes/authors')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayout)
 app.use(express.static('public'))
 app.use('/', indexRouter)
+app.use('/authors', authorsRouter)
+
+
+
+
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
